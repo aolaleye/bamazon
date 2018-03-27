180fd8d3b042ba156038ffb94fe4e25fa3c1553d
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-03-27 15:13:47 -0400

    added mysql data

---
+++ schema.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE database bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products (
+  item_id INT,
+  product_name VARCHAR(100),
+  department_name VARCHAR(100),
+  price DECIMAL(4,3),
+  stock_quantity INT,
+  PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
