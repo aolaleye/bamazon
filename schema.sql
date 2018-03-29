cc4d71e07e0647b94b651f792f1895acab010fe8
aolaleye <30451506+aolaleye@users.noreply.github.com>
2018-03-29 19:05:57 -0400

    updated mysql database and data with product_sales column with a default of 0

--- schema.sql
+++ schema.sql
@@ -7,8 +7,9 @@ CREATE TABLE products (
   item_id INT,
   product_name VARCHAR(100),
   department_name VARCHAR(100),
-  price DECIMAL(4,2),
+  price DECIMAL(10,2),
   stock_quantity INT,
+  product_sales DECIMAL(10,2) DEFAULT 0,
   PRIMARY KEY (item_id)
 );
 
